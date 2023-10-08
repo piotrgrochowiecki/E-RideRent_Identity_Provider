348eb5a0de205a41696efc8116d7d1f7ca82fc8d
Piotr Grochowiecki <Piotr.Grochowiecki@dxc.com>
2023-10-08 20:28:32 +0200

    initial commit

---
+++ src/main/java/com/piotrgrochowiecki/eriderent_identity_provider/ERideRentIdentityProviderApplication.java
@@ -0,0 +1,13 @@
+package com.piotrgrochowiecki.eriderent_identity_provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ERideRentIdentityProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ERideRentIdentityProviderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/piotrgrochowiecki/eriderent_identity_provider/ERideRentIdentityProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piotrgrochowiecki.eriderent_identity_provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ERideRentIdentityProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
