406ccf54fe773bac0dd003224205c0b6faaf93f5
piotrgrochowiecki <piotrgrochowiecki@gmail.com>
2023-11-30 15:48:46 +0100

    added handling of ExpiredJwtException

--- src/main/java/com/piotrgrochowiecki/eriderent_identity_provider/remote/controller/GlobalExceptionHandler.java
+++ src/main/java/com/piotrgrochowiecki/eriderent_identity_provider/remote/controller/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.piotrgrochowiecki.eriderent_identity_provider.remote.controller;
 import com.piotrgrochowiecki.eriderent_identity_provider.domain.exception.BadCredentialsRuntimeException;
 import com.piotrgrochowiecki.eriderent_identity_provider.domain.exception.NotFoundRuntimeException;
 import com.piotrgrochowiecki.eriderent_identity_provider.remote.dto.RuntimeExceptionDto;
+import io.jsonwebtoken.ExpiredJwtException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,6 +21,16 @@ public class GlobalExceptionHandler {
     public RuntimeExceptionDto handleBadCredentialsRuntimeException(BadCredentialsRuntimeException exception) {
         return RuntimeExceptionDto.builder()
                 .message(exception.getMessage())
+                .timeStamp(LocalDateTime.now())
+                .build();
+    }
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler(ExpiredJwtException.class)
+    public RuntimeExceptionDto handleExpiredJwtRuntimeException() {
+        return RuntimeExceptionDto.builder()
+                .message("Token has expired")
                 .timeStamp(LocalDateTime.now())
                 .build();
     }
