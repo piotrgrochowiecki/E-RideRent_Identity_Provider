cdb2936f2d58f2eb2158d016ed4c6ea41f9bd7e7
Piotr Grochowiecki <42697061+piotrgrochowiecki@users.noreply.github.com>
2023-12-01 15:54:18 +0100

    added /api/v1/internal/user/id/ endpoint to adminAllowedEndpoints (#9)

--- src/main/java/com/piotrgrochowiecki/eriderent_identity_provider/domain/service/RoleAndAllowedEndpointMapProvider.java
+++ src/main/java/com/piotrgrochowiecki/eriderent_identity_provider/domain/service/RoleAndAllowedEndpointMapProvider.java
@@ -19,16 +19,17 @@ public class RoleAndAllowedEndpointMapProvider {
     @PostConstruct
     public void init() {
         List<Endpoint> customerAllowedEndpoints = List.of(new Endpoint("/api/v1/internal/user/uuid/", HttpMethod.GET),
-                                                          new Endpoint("/api/v1/internal/booking/", HttpMethod.GET),
-                                                          new Endpoint("/api/v1/internal/booking/all-overlapping-with-dates/", HttpMethod.GET),
+                                                          new Endpoint("/api/v1/internal/booking/id", HttpMethod.GET),
+                                                          new Endpoint("/api/v1/internal/booking/all-overlapping-with-dates", HttpMethod.GET),
                                                           new Endpoint("/api/v1/car/", HttpMethod.GET),
                                                           new Endpoint("/api/v1/car/available/", HttpMethod.GET));
 
         List<Endpoint> adminAllowedEndpoints = List.of(new Endpoint("/api/v1/internal/user/uuid/", HttpMethod.GET),
+                                                       new Endpoint("/api/v1/internal/user/id/", HttpMethod.GET),
                                                        new Endpoint("/api/v1/internal/user/email/", HttpMethod.GET),
-                                                       new Endpoint("/api/v1/internal/booking/", HttpMethod.GET),
-                                                       new Endpoint("/api/v1/internal/booking/all/", HttpMethod.GET),
-                                                       new Endpoint("/api/v1/internal/booking/all-overlapping-with-dates/", HttpMethod.GET),
+                                                       new Endpoint("/api/v1/internal/booking/id/", HttpMethod.GET),
+                                                       new Endpoint("/api/v1/internal/booking/all", HttpMethod.GET),
+                                                       new Endpoint("/api/v1/internal/booking/all-overlapping-with-dates", HttpMethod.GET),
                                                        new Endpoint("/api/v1/car/", HttpMethod.GET),
                                                        new Endpoint("/api/v1/car/", HttpMethod.POST),
                                                        new Endpoint("/api/v1/car/available/", HttpMethod.GET));
